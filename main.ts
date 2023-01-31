08d44562b22f6393c89c779fe70fc642a06f5b03
ellagroff <122560331+ellagroff@users.noreply.github.com>
2023-01-31 13:30:03 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,15 +8,33 @@ function UpdateScoreBoard () {
     OLED.newLine()
     OLED.writeStringNewLine("Rounds:" + Rounds)
 }
+input.onButtonPressed(Button.A, function () {
+    PA += 1
+    Rounds += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    Ties += 1
+    Rounds += 1
+})
+input.onButtonPressed(Button.B, function () {
+    PB += 1
+    Rounds += 1
+})
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("Ready to play?")
+    PA = 0
+    PB = 0
+    Ties = 0
+    Rounds = 0
+    basic.pause(2000)
+    UpdateScoreBoard()
+}
 let Rounds = 0
 let Ties = 0
 let PB = 0
 let PA = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("Ready to play?")
-PA = 0
-PB = 0
-Ties = 0
-Rounds = 0
-basic.pause(2000)
-UpdateScoreBoard()
+reset()
